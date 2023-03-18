f6c1db024a25546f7a8d81d3501c10a4192edbfc
Nifont <nifontbus@yandex.ru>
2023-03-18 23:52:41 +0300

    resent multipart files

--- rest/src/main/kotlin/ru/nb/medalist/msuser/rest/controller/UserController.kt
+++ rest/src/main/kotlin/ru/nb/medalist/msuser/rest/controller/UserController.kt
@@ -1,16 +1,18 @@
 package ru.nb.medalist.msuser.rest.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import ru.nb.medalist.msuser.domain.TestData
+import kotlinx.coroutines.reactive.collect
+import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
 import ru.nb.medalist.msuser.domain.bussines.processor.UserProcessor
 import ru.nb.medalist.msuser.domain.model.User
 import ru.nb.medalist.msuser.rest.controller.base.process
 import ru.nb.medalist.msuser.rest.controller.mappers.fromTransport
 import ru.nb.medalist.msuser.rest.controller.mappers.toTransportGetUser
 import ru.nb.medalist.msuser.rest.controller.request.CreateOwnerRequest
+import java.nio.file.Paths
+import java.util.*
 
 @RestController
 @RequestMapping
@@ -27,6 +29,44 @@ class UserController(
 			fromTransport = { fromTransport(it) },
 			toTransport = { toTransportGetUser() }
 		)
+	}
+
+	@PostMapping(value = ["/upload"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+	fun upload(@RequestPart("file") filePart: Mono<FilePart>): Mono<String> {
+
+		return filePart
+			.doOnNext { fp ->
+				println("Received File : " + fp.filename())
+			}
+			.flatMap { fp ->
+				fp.transferTo(basePath.resolve(fp.filename()))
+			}
+			.map { "OK" }
+//			.then ()
+
+	}
+
+	@PostMapping(value = ["/up"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+	suspend fun up(@RequestPart("file") filePart: Mono<FilePart>): String {
+
+		filePart
+			.doOnNext { fp ->
+				println("Received File : " + fp.filename())
+
+			}
+			.flatMap { fp ->
+				fp.transferTo(basePath.resolve(fp.filename()))
+			}
+//			.subscribe()
+			.collect { }
+
+		return "OK"
+
+	}
+
+	companion object {
+		private const val LOCAL_FOLDER = "d:/files"
+		private val basePath = Paths.get(LOCAL_FOLDER)
 	}
 
 }
